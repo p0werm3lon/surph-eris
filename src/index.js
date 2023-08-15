daae5ce86dce50e24fa824d5eb9fa739d4b65e1c
vvda <vvdnet@pm.me>
2023-08-16 00:17:00 +0100

    Fuck

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const signale_1 = __importDefault(require("signale"));
-signale_1.default.fatal('test');
-//# sourceMappingURL=index.js.map
