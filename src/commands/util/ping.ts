0947bfb2479102e47a59dcee95f031515bfe7509
vvda <vvdnet@pm.me>
2023-08-17 18:06:42 +0100

    Update version & update ping command

--- src/commands/util/ping.ts
+++ src/commands/util/ping.ts
@@ -1,9 +1,12 @@
 import Command from "../../classes/Command";
 import { reply } from "../../modules/util";
 
+import pkg from '../../../package.json';
+import * as os from 'node:os';
+
 export default {
     name: 'ping',
     run: async (msg) => {
-        reply('yoooo', msg);
+        reply({embed: {color: 0xFFFFFF, title: 'Pong!', type: 'rich', fields: [{name: 'Version', value: `\`${pkg.version}\``}, {name: 'Server', value: `\`${os.hostname()}\``}]}}, msg);
     }, options: { aliases: ['pong'] }
 } as Command
